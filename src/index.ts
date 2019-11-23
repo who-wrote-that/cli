902899fb54fcefe46a37f070903daeeca1d45f07
Anton Baumann <antonbaumann4@gmail.com>
2019-11-23 03:06:18 +0100

    git show

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,16 @@ import {findSpans, findDef} from './parse'
 
 
 commander
-  .version('Codeowners 0.1.0', '-v, --version')
-  .option('-d, --debug', 'enable debug mode')
-  .arguments('<file> <line>')
-  .description('...')
-  .action((file, line, options) => {
-      readFile(path.join(process.cwd(), file)).then(data => {
-          console.log(findDef(data, line));
-          console.log(findSpans(data, 'Test'));
-      })
-  });
+    .version('Codeowners 0.1.0', '-v, --version')
+    .option('-d, --debug', 'enable debug mode')
+    .arguments('<file> <line>')
+    .description('...')
+    .action((file, line, options) => {
+        readFile(path.join(process.cwd(), file)).then(data => {
+            console.log(findDef(data, line));
+            console.log(findSpans(data, 'Test'));
+
+        })
+    });
 
 commander.parse(process.argv);
