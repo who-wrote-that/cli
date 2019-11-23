d0c08c1a8a2acfaf6597f3691e7d5128c0e023d8
Anton Baumann <antonbaumann4@gmail.com>
2019-11-23 01:56:00 +0100

    find method by line or method

---
+++ src/util.ts
@@ -0,0 +1,9 @@
+import fs from 'fs'
+
+export const readFile = (path: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            !err ? resolve(data) : reject(err)
+        })
+    })
+};
