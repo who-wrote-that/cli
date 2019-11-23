2f79aeec6e4e5204fa8d8a72dfec072bc83c36ca
Anton Baumann <antonbaumann4@gmail.com>
2019-11-23 11:44:19 +0100

    refactored git-diff

--- src/git.ts
+++ src/git.ts
@@ -12,20 +12,33 @@ const getChangedLineNumbersAtCommit = (filePath: string, commitHash: string): Pr
             `git diff -U0 ${commitHash}~ ${commitHash} ./${filePath}`,
             (err, data) => {
                 if (!err) {
-                    childProcess.exec(`git diff -U0 ${commitHash}~ ${commitHash} ./${filePath} | diff-lines`,
-                        (err, data) => {
-                            !err ? resolve(data.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))): reject(err)
+                    getDiff(`git diff -U0 ${commitHash}~ ${commitHash} ./${filePath} | diff-lines`)
+                        .then(lineNumbers => resolve(lineNumbers))
+                        .catch(err => {
+                            console.error(err);
+                            process.exit(1);
                         })
                 } else {
-                    childProcess.exec(
-                        `git show ${commitHash} ./${filePath} | diff-lines`,
-                        (err, data) => {
-                            !err ? resolve(data.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))): reject(err)
+                    getDiff(`git show ${commitHash} ./${filePath} | diff-lines`)
+                        .then(lineNumbers => resolve(lineNumbers))
+                        .catch(err => {
+                            console.error(err);
+                            process.exit(1);
                         })
                 }
             }
         )
     })
+};
+
+const getDiff = (command: string): Promise<number[]> => {
+    return new Promise((resolve, reject) => {
+        childProcess.exec(command,
+            (err, data) => {
+                !err ? resolve(data.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))) : reject(err)
+            })
+        }
+    )
 };
 
 const getAuthorOfCommit = (commitHash: string): Promise<string> => {
@@ -58,10 +71,10 @@ export const readFileAtCommit = (filePath: string, commitHash: string): Promise<
 export const getOwnerOfCommit = (filePath: string, commitHash: string, span: Span): Promise<Owner> => {
     return Promise.all([getAuthorOfCommit(commitHash), getChangedLineNumbersAtCommit(filePath, commitHash)])
         .then(([author, lineNumbers]) => {
-           return {
-               author,
-               score: lineNumbers.filter(n => n >= span.from && n <= span.to).length,
-           }
+            return {
+                author,
+                score: lineNumbers.filter(n => n >= span.from && n <= span.to).length,
+            }
         }).catch(err => {
             console.error(err);
             process.exit(1);
