3ef81a91fa2fb31a9b3cf79b219470da1c7d6067
Anton Baumann <antonbaumann4@gmail.com>
2019-11-23 11:33:57 +0100

    filter commits

--- src/git.ts
+++ src/git.ts
@@ -6,19 +6,19 @@ export type Owner = {
     score: number;
 }
 
-const getChangedLineNumbersAtCommit = (filePath: string, commitIndex: number): Promise<number[]> => {
+const getChangedLineNumbersAtCommit = (filePath: string, commitHash: string): Promise<number[]> => {
     return new Promise((resolve, reject) => {
         childProcess.exec(
-            `git diff -U0 HEAD~${commitIndex+1} HEAD~${commitIndex} ./${filePath}`,
+            `git diff -U0 ${commitHash}~ ${commitHash} ./${filePath}`,
             (err, data) => {
                 if (!err) {
-                    childProcess.exec(`git diff -U0 HEAD~${commitIndex+1} HEAD~${commitIndex} ./${filePath} | diff-lines`,
+                    childProcess.exec(`git diff -U0 ${commitHash}~ ${commitHash} ./${filePath} | diff-lines`,
                         (err, data) => {
                             !err ? resolve(data.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))): reject(err)
                         })
                 } else {
                     childProcess.exec(
-                        `git show HEAD~${commitIndex} ./${filePath} | diff-lines`,
+                        `git show ${commitHash} ./${filePath} | diff-lines`,
                         (err, data) => {
                             !err ? resolve(data.split('\n').map(n => parseInt(n)).filter(n => !isNaN(n))): reject(err)
                         })
@@ -28,26 +28,35 @@ const getChangedLineNumbersAtCommit = (filePath: string, commitIndex: number): P
     })
 };
 
-const getAuthorOfCommit = (commitIndex: number): Promise<string> => {
+const getAuthorOfCommit = (commitHash: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         childProcess.exec(
-            `git show HEAD~${commitIndex} | grep Author`,
+            `git show ${commitHash} | grep Author`,
             (err, data) => !err ? resolve(data.substring(data.indexOf(' ') + 1).split('\n')[0]) : reject(err)
         )
     })
 };
 
-export const readFileAtCommit = (filePath: string, commitIndex: number): Promise<string> => {
+export const commitsAffectingFile = (filePath: string): Promise<string[]> => {
     return new Promise((resolve, reject) => {
         childProcess.exec(
-            `git show HEAD~${commitIndex}:./${filePath}`,
+            `git log --follow ./${filePath} | grep commit`,
+            (err, data) => !err ? resolve(data.split('\n').map(line => line.replace('commit ', ''))) : reject(err)
+        )
+    })
+};
+
+export const readFileAtCommit = (filePath: string, commitHash: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        childProcess.exec(
+            `git show ${commitHash}:./${filePath}`,
             (err, data) => !err ? resolve(data) : reject(err)
         )
     })
 };
 
-export const getOwnerOfCommit = (filePath: string, commitIndex: number, span: Span): Promise<Owner> => {
-    return Promise.all([getAuthorOfCommit(commitIndex), getChangedLineNumbersAtCommit(filePath, commitIndex)])
+export const getOwnerOfCommit = (filePath: string, commitHash: string, span: Span): Promise<Owner> => {
+    return Promise.all([getAuthorOfCommit(commitHash), getChangedLineNumbersAtCommit(filePath, commitHash)])
         .then(([author, lineNumbers]) => {
            return {
                author,
